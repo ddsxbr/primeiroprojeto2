c24b787524b506733674681ffb2a461f4cdff0b3
Eduardo Freiberger <eduardo.silva.freiberger@gmail.com>
2025-04-03 01:36:39 -0300

    git commit -m "Mensagem do commit" git push origin main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Alô Mundo!");
+        System.out.println("----------------------");
+
+        PrimeiraClasse.executar();
+    }
+}
+class PrimeiraClasse {
+    public static void executar () {
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+    }
+}
